8a4b5d7516b0a91100dca1aed4fb0a375e0e5f55
TheCatfishUK <106726064+TheCatfishUK@users.noreply.github.com>
2023-12-05 07:44:24 +0000

    Update code.py

--- Day3/code.py
+++ Day3/code.py
@@ -5,3 +5,33 @@ if data == 0:
 else:
     f = open("input.py")
 
+grid = []
+
+for line in f:
+    line = line.strip()
+    gridline = []
+    for a in range(len(line)):
+        gridline.append({"id":line[a], "c":0, "s":0})
+    grid.append(gridline)
+
+# Who knows what I'm actually doing here
+
+for a in range(len(grid)):
+    for b in range(len(grid[a])):
+        if grid[a][b]['c'] == 0:
+            # print(grid[a][b]['c'])
+            for i in range(-1,2):
+                for j in range(-1,2):
+                    try:
+                        grid[a+i][b+j]['c']
+                    except:
+                        pass
+                    else:
+                        try:
+                            int(grid[a+i][b+j]['id'])
+                        except:
+                            if grid[a+i][b+j]['id'] != '.':
+                                grid[a][b]['s'] = 1
+
+for a in grid:
+    print(a)
